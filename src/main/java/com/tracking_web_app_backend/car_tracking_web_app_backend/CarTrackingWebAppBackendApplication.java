efe40ca177cbc27cfa1f92ad032167f16573ae6a
Jotham Zvikonyaukwa <jotham.zvikonya@gmail.com>
2025-03-08 21:26:58 -0500

    car tracking web app temoplate

---
+++ src/main/java/com/tracking_web_app_backend/car_tracking_web_app_backend/CarTrackingWebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tracking_web_app_backend.car_tracking_web_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTrackingWebAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarTrackingWebAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tracking_web_app_backend/car_tracking_web_app_backend/CarTrackingWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracking_web_app_backend.car_tracking_web_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackingWebAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
